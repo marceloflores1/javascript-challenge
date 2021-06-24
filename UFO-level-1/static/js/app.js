d163fb7ccab45579df7ae0d26c4956cdff8b2f1e
Marcelo Flores <79871752+marceloflores1@users.noreply.github.com>
2021-06-23 23:14:43 -0500

    Modyfing app.js with functions and index.html with format

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,68 @@
+// Importing from data.js
+var tableData = data;
+
+// Selecting 
+var form = d3.select("#filters");
+var filterButton = d3.select("#filter-btn");
+var allButton = d3.select("#all-btn");
+var clearButton = d3.select("#clear-btn");
+
+// Event handlers
+form.on("submit", runFilter);
+filterButton.on("click", runFilter);
+allButton.on("click", runAll);
+clearButton.on("click", runClear);
+
+
+// Function runFilter
+function runFilter() {
+    // d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    var filteredUfo = tableData.filter(date => (date.datetime === inputValue));
+
+    if ( filteredUfo.length !== 0 ) {
+        console.log(filteredUfo);
+        var ufoTbody = d3.select("#ufo-tbody");
+        ufoTbody.html("");
+        filteredUfo.forEach(ufo => {
+            var row = ufoTbody.append("tr");
+            row.append("td").text(ufo.datetime);
+            row.append("td").text(ufo.city);
+            row.append("td").text(ufo.state);
+            row.append("td").text(ufo.country);
+            row.append("td").text(ufo.shape);
+            row.append("td").text(ufo.durationMinutes);
+            row.append("td").text(ufo.comments);
+        })
+    } else {
+        console.log(`Not found`);
+        var ufoTbody = d3.select("#ufo-tbody");
+        ufoTbody.html("");
+    }
+}
+
+function runAll() {
+    // d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+}
+
+function runClear() {
+    // d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+}
